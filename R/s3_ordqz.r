28f3cdedf1c842921be21da81b6af85b0b23328f
wccsnow <wccsnow@gmail.com>
2019-11-23 13:10:00 -0500

    ordqz

--- R/s3_ordqz.r
+++ R/s3_ordqz.r
@@ -1,22 +1,31 @@
 ### S3 functions for generalized eigen values.
 
 ordqz <- function(A, B = NULL, cluster = NULL,
-    keyword = c("lhp", "rhp", "udi", "udo", "ref", "cef"), ...){
+    keyword = c("lhp", "rhp", "udi", "udo", "ref", "cef",
+                "lhp.fo", "rhp.fo", "udi.fo", "udo.fo"),
+    ...){
+  ### All keywords for sorting/ording/clustering the generalized eigen values.
+  # lhp: Left-half (real(E) < 0)
+  # rhp: Right-half (real(E) >= 0)
+  # udi: Interior of unit disk (abs(E) < 1)
+  # udo: Exterior of unit disk (abs(E) >= 1)
+  # ref: Real eigenvalues first (top-left conner)
+  # cef: Complex eigenvalues first (top-left conner)
+  # lhp.fo: Left-half (real(E) < 0) and finite only
+  # rhp.fo: Right-half (real(E) >= 0) and finite only
+  # udi.fo: Interior of unit disk (abs(E) < 1) and finite only
+  # udo.fo: Exterior of unit disk (abs(E) >= 1) and finite only
+  all.keywords <- c("lhp", "rhp", "udi", "udo", "ref", "cef",
+                    "lhp.fo", "rhp.fo", "udi.fo", "udo.fo")
+
   if(!is.null(cluster)){
     select <- cluster
     only.values <- FALSE
     ret <- qz(A = A, B = B, select = select, only.values = only.values, ...)
   } else{
-    # lhp: Left-half (real(E) < 0)
-    # rhp: Right-half (real(E) > 0)
-    # udi: Interior of unit disk (abs(E) < 1)
-    # udo: Exterior of unit disk (abs(E) > 1)
-    # ref: Real eigenvalues first (top-left conner)
-    # cef: Complex eigenvalues first (top-left conner)
-    keyword <- match.arg(tolower(keyword[1]),
-                         c("lhp", "rhp", "udi", "udo", "ref", "cef"))
-    if(!(keyword[1] %in% c("lhp", "rhp", "udi", "udo", "ref", "cef"))){
-      stop("keyword should be one of lhp, rhp, udi, udo, ref, or cef.")
+    keyword <- match.arg(tolower(keyword[1]), all.keywords)
+    if(!(keyword[1] %in% all.keywords)){
+      stop("keyword is unknown.")
     }
   
     # Do usual qz without select.
@@ -36,8 +45,7 @@ ordqz <- function(A, B = NULL, cluster = NULL,
       } else if(class.ret %in% c("zgees", "dgees")){
         tmp.id <- Re(ret$W)
       }
-      select[tmp.id < 0 & is.finite(tmp.id)] <- TRUE
-
+      select[tmp.id < 0] <- TRUE
     } else if(keyword == "rhp"){
       # rhp: Right-half (real(E) > 0)
       if(class.ret %in% c("zgges", "dgges")){
@@ -45,7 +53,7 @@ ordqz <- function(A, B = NULL, cluster = NULL,
       } else if(class.ret %in% c("zgees", "dgees")){
         tmp.id <- Re(ret$W)
       }
-      select[tmp.id > 0 & is.finite(tmp.id)] <- TRUE
+      select[tmp.id >= 0] <- TRUE
     } else if(keyword == "udi"){
       # udi: Interior of unit disk (abs(E) < 1)
       if(class.ret %in% c("zgges", "dgges")){
@@ -53,7 +61,7 @@ ordqz <- function(A, B = NULL, cluster = NULL,
       } else if(class.ret %in% c("zgees", "dgees")){
         tmp.id <- Mod(ret$W)
       }
-      select[tmp.id < 1 & is.finite(tmp.id)] <- TRUE
+      select[tmp.id < 1] <- TRUE
     } else if(keyword == "udo"){
       # udo: Exterior of unit disk (abs(E) > 1)
       if(class.ret %in% c("zgges", "dgges")){
@@ -61,7 +69,39 @@ ordqz <- function(A, B = NULL, cluster = NULL,
       } else if(class.ret %in% c("zgees", "dgees")){
         tmp.id <- Mod(ret$W)
       }
-      select[tmp.id > 1 & is.finite(tmp.id)] <- TRUE
+      select[tmp.id >= 1] <- TRUE
+    } else if(keyword == "lhp.fo"){
+      # lhp: Left-half (real(E) < 0)
+      if(class.ret %in% c("zgges", "dgges")){
+        tmp.id <- Re(ret$ALPHA / ret$BETA)
+      } else if(class.ret %in% c("zgees", "dgees")){
+        tmp.id <- Re(ret$W)
+      }
+      select[tmp.id < 0 & is.finite(tmp.id)] <- TRUE
+    } else if(keyword == "rhp.fo"){
+      # rhp: Right-half (real(E) > 0)
+      if(class.ret %in% c("zgges", "dgges")){
+        tmp.id <- Re(ret$ALPHA / ret$BETA)
+      } else if(class.ret %in% c("zgees", "dgees")){
+        tmp.id <- Re(ret$W)
+      }
+      select[tmp.id >= 0 & is.finite(tmp.id)] <- TRUE
+    } else if(keyword == "udi.fo"){
+      # udi: Interior of unit disk (abs(E) < 1)
+      if(class.ret %in% c("zgges", "dgges")){
+        tmp.id <- Mod(ret$ALPHA / ret$BETA)
+      } else if(class.ret %in% c("zgees", "dgees")){
+        tmp.id <- Mod(ret$W)
+      }
+      select[tmp.id < 1 & is.finite(tmp.id)] <- TRUE
+    } else if(keyword == "udo.fo"){
+      # udo: Exterior of unit disk (abs(E) > 1)
+      if(class.ret %in% c("zgges", "dgges")){
+        tmp.id <- Mod(ret$ALPHA / ret$BETA)
+      } else if(class.ret %in% c("zgees", "dgees")){
+        tmp.id <- Mod(ret$W)
+      }
+      select[tmp.id >= 1 & is.finite(tmp.id)] <- TRUE
     } else if(keyword == "ref"){
       # ref: Real eigenvalues first (top-left conner)
       if(class.ret %in% c("zgges", "dgges")){
