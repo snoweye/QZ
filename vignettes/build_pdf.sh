be794196aec35ebc7e0e13b7bb7c57e5b81d0bed
wccsnow <wccsnow@gmail.com>
2020-12-12 12:35:27 -0500

    Fix jss problems

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-rm *.aux *.bbl *.blg *.log *.out *.toc
+R_HOME=`Rscript -e 'cat(R.home()[1])'`
+JSS_BST=${R_HOME}/share/texmf/bibtex/bst/jss.bst
+JSS_CLS=${R_HOME}/share/texmf/tex/latex/jss.cls
+
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
+cp ${JSS_BST} ./
+cp ${JSS_CLS} ./
+
 pdflatex QZ-guide.Rnw
 bibtex QZ-guide
 pdflatex QZ-guide.Rnw
 pdflatex QZ-guide.Rnw
 pdflatex QZ-guide.Rnw
-rm *.aux *.bbl *.blg *.log *.out *.toc
+Rscript -e "tools::compactPDF('.', gs_quality='ebook')"
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
 
-mv -f *.pdf ../inst/doc/
+qpdf QZ-guide.pdf ../inst/doc/QZ-guide.pdf
+# mv -f *.pdf ../inst/doc/
+rm -f *.pdf
 cp -f *.Rnw ../inst/doc/
